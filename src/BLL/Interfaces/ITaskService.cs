66ec576eb3c8d758a6567a24d488af9625b1cfcd
shabs <43846967+shabs82@users.noreply.github.com>
2021-03-23 11:41:15 +0100

    all assignee tests done

--- src/BLL/Interfaces/ITaskService.cs
+++ src/BLL/Interfaces/ITaskService.cs
@@ -8,6 +8,6 @@ namespace BLL.Interfaces
 {
     public interface ITaskService
     {
-        Task 
+       
     }
 }
--- src/TestProject/UnitTest1.cs
+++ src/TestProject/UnitTest1.cs
@@ -4,6 +4,7 @@ using Moq;
 using System;
 using BLL.Interfaces;
 using FluentAssertions;
+using Model;
 using Xunit;
 
 namespace TestProject
@@ -28,6 +29,23 @@ namespace TestProject
         }
 
         [Fact]
+        public void ReadAll_withNegativeId_ShouldThrowExeption_Once()
+        {
+            //arrange
+            var assigneeRepositoryMock = new Mock<IAssigneeRepository>();
+           IAssigneeService service = new AssigneeService(assigneeRepositoryMock.Object);
+           Assignee mockAssignee = new Assignee() {Id = -1};
+            //assign
+            assigneeRepositoryMock.Setup(rm => rm.ReadAll(-1));
+            service.ReadAll(-1);
+            //assert
+           assigneeRepositoryMock.Verify(rm => rm.ReadAll(-1), Times.Once);
+
+
+        }
+
+
+        [Fact]
         public void ReadAll_ShouldMatchReadAllInRepository_Once()
         {
             //arrange
@@ -39,6 +57,21 @@ namespace TestProject
             //assert
             AssigneeRepositoryMock.Verify(r => r.ReadAll(50), Times.Once());
             CheckPerformance(() => service.ReadAll(50), 1000);
+        }
+        [Fact]
+        public void Delete_ShouldCallDeleteMethodInRepository_withParamAsId_Once()
+        {
+            //arrange
+            var assigneeRepositoryMock = new Mock<IAssigneeRepository>();
+           
+            IAssigneeService service = new AssigneeService(assigneeRepositoryMock.Object);
+            //act
+            var deletedAssignee = new Assignee() {Id = 3, Name = "" };
+          assigneeRepositoryMock.Setup(r => r.DeleteAssignee(3)).Returns(() => deletedAssignee);
+            service.DeleteAssignee(3);
+            //assert
+           assigneeRepositoryMock.Verify(r => r.DeleteAssignee(3), Times.Once());
+            CheckPerformance(() => service.DeleteAssignee(3), 1000);
         }
     }
 }
