620327a377f8b4712bcd73b96672412f68fc8d45
shabs <43846967+shabs82@users.noreply.github.com>
2021-03-24 14:47:24 +0100

    service added

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BLL;
+using BLL.Interfaces;
 using DAL;
 using DAL.Interfaces;
 using Microsoft.AspNetCore.Builder;
@@ -54,34 +56,35 @@ namespace API
 
                     opt.UseSqlServer(Configuration.GetConnectionString("defaultConnection"));
                 });
-            //    services.AddDbContext<TodoContext>(b => b
-            //        .UseSqlServer(Environment.GetEnvironmentVariable("DatabaseConnectionString"))
-            //        .LogTo(Console.WriteLine)
-            //}
-            
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"}); });
-            
-            services.AddScoped<IAssigneeRepository, AssigneeRepository>();
-            services.AddScoped<ITaskRepository, TaskRepository>();
-            services.AddTransient<DataInit>();
+                //    services.AddDbContext<TodoContext>(b => b
+                //        .UseSqlServer(Environment.GetEnvironmentVariable("DatabaseConnectionString"))
+                //        .LogTo(Console.WriteLine)
+                //}
 
-            services.AddCors(options =>
-                options.AddDefaultPolicy(
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-                    })
-            );
+                services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"}); });
 
-            services.AddControllers();
-            services.AddMvc().AddNewtonsoftJson();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-            services.AddControllers().AddNewtonsoftJson(options =>
-            {    // Use the default property (Pascal) casing
-                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-                options.SerializerSettings.MaxDepth = 5;
-            });
+                services.AddScoped<IAssigneeRepository, AssigneeRepository>();
+                services.AddScoped<IAssigneeService, AssigneeService>();
+                services.AddScoped<ITaskRepository, TaskRepository>();
+                services.AddScoped<ITaskService, TaskService>();
+                services.AddTransient<DataInit>();
 
+                services.AddCors(options =>
+                    options.AddDefaultPolicy(
+                        builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); })
+                );
+
+                services.AddControllers();
+                services.AddMvc().AddNewtonsoftJson();
+                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+                services.AddControllers().AddNewtonsoftJson(options =>
+                {
+                    // Use the default property (Pascal) casing
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.MaxDepth = 5;
+                });
+
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
