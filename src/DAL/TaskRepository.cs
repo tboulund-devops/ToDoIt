8322a5d284a37a2bb6db441eb4290b626ebd1f6b
math231k <54638811+math231k@users.noreply.github.com>
2021-04-08 18:40:13 +0200

    Repository created and create implemented

---
+++ src/DAL/TaskRepository.cs
@@ -0,0 +1,43 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL
+{
+    public class TaskRepository : IRepository<Task>
+    {
+        private readonly TodoContext _ctx;
+        public TaskRepository(TodoContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public Task Create(Task entity)
+        {
+            var task = _ctx.Tasks.Add(entity);
+            _ctx.SaveChanges();
+            return task.Entity;
+        }
+
+        public Task Delete(Task entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Task> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Task entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
