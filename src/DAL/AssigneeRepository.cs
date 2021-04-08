731cc443fae033410663e7d87c8771ed0805d399
math231k <54638811+math231k@users.noreply.github.com>
2021-04-08 18:45:21 +0200

    Assignee Repo created and create implemented

---
+++ src/DAL/AssigneeRepository.cs
@@ -0,0 +1,42 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+
+namespace DAL
+{
+    class AssigneeRepository : IRepository<Assignee>
+    {
+        private readonly TodoContext _ctx;
+
+        public AssigneeRepository(TodoContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public Assignee Create(Assignee entity)
+        {
+            var assignee = _ctx.Assignees.Add(entity);
+            _ctx.SaveChanges();
+            return assignee.Entity;
+        }
+
+        public Assignee Delete(Assignee entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Assignee Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Assignee> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Assignee Update(Assignee entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
