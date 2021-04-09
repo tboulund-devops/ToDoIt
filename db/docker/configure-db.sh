848fd867bd008caddbd40f646beb482a5f661329
Troels <31724037+tr0els@users.noreply.github.com>
2021-04-09 16:56:58 +0200

    MsSql longer timeout

--- db/docker/configure-db.sh
+++ db/docker/configure-db.sh
@@ -4,14 +4,14 @@
 export STATUS=1
 i=0
 
-while [[ $STATUS -ne 0 ]] && [[ $i -lt 30 ]]; do
+while [[ $STATUS -ne 0 ]] && [[ $i -lt 120 ]]; do
 	i=$i+1
 	/opt/mssql-tools/bin/sqlcmd -t 1 -U sa -P $SA_PASSWORD -Q "select 1" >> /dev/null
 	STATUS=$?
 done
 
 if [ $STATUS -ne 0 ]; then 
-	echo "Error: MSSQL SERVER took more than thirty seconds to start up."
+	echo "Error: MSSQL SERVER took more than 120 seconds to start up."
 	exit 1
 fi
 
